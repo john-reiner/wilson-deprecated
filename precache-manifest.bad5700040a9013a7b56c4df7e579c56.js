c81c3491b28db371196ff0df28fd6cc93aeb8f15
john-reiner <johnreiner1990@gmail.com>
2020-08-12 22:59:06 -0400

    Updates

---
+++ precache-manifest.bad5700040a9013a7b56c4df7e579c56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d908acd92ad944e96a106cb3ae7e128",
+    "url": "/wilson/index.html"
+  },
+  {
+    "revision": "adecad4d427c12f6859a",
+    "url": "/wilson/static/css/2.25b83785.chunk.css"
+  },
+  {
+    "revision": "8c1d82a383666dfeda0d",
+    "url": "/wilson/static/css/main.33d34858.chunk.css"
+  },
+  {
+    "revision": "adecad4d427c12f6859a",
+    "url": "/wilson/static/js/2.20c1dea5.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/wilson/static/js/2.20c1dea5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c1d82a383666dfeda0d",
+    "url": "/wilson/static/js/main.a8683d5e.chunk.js"
+  },
+  {
+    "revision": "88902aeab9ee0069d9e8",
+    "url": "/wilson/static/js/runtime-main.a09e9515.js"
+  }
+]);
